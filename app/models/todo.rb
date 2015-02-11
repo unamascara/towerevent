424d4e80d9c8cf7bd83d608d164b738245b9907e
unamascara <soyunamascara@gmail.com>
2015-02-11 18:04:05 +0800

    create event when remove, start, pause and complete a todo. tested

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -23,14 +23,25 @@ class Todo < ActiveRecord::Base
 
   def create_event
 
-      self.changed_attributes.each {|attr,value|
+    self.changed_attributes.each {|attr,value|
 
-        case attr
-          when 'id'
-            Event.create!(body:'created todo',eventable:self)
-          when 'body'
-        end
-      }
+      case attr
+        when 'id'
+          Event.create!(body:'created todo',eventable:self)
+        when 'status'
+          case self.status
+            when 0
+              Event.create!(body:'paused todo',eventable:self) if value==1
+            when 1
+              Event.create!(body:'started todo',eventable:self)
+            when 2
+              Event.create!(body:'finished todo',eventable:self)
+            else
+              Event.create!(body:'removed todo',eventable:self)
+          end
+      end
+
+    }
   end
 
   def url
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,16 +5,59 @@ RSpec.describe Event, :type => :model do
   let(:team){Team.create!(name:'testTeam')}
   let(:user){User.create!(name:'testUser',team:team)}
   let(:project){Project.create!(name:'testProject',team:team)}
+  let(:todo){Todo.create!(body:'have a small talk',project:project,status:0)}
   describe 'event for todos' do
     it 'create a event when create a todo' do
-      todo=Todo.create!(body:'have a small talk',project:project,status:0)
 
+      todo
       event = Event.first
       expect(event.body).to eq('created todo')
       expect(event.eventable).to eq(todo)
       expect(event.eventable.url).to eq('/todos/1')
 
     end
+
+    it 'create a event when remove a todo' do
+
+      todo.status=3
+      todo.save!
+      event = Event.last
+      expect(event.body).to eq('removed todo')
+
+    end
+
+    it 'create a event when complete a todo' do
+
+      todo.status=2
+      todo.save!
+      event = Event.last
+      expect(event.body).to eq('finished todo')
+
+    end
+
+
+    it 'create a event when start a todo' do
+
+      todo.status=1
+      todo.save!
+      event = Event.last
+      expect(event.body).to eq('started todo')
+
+    end
+
+    it 'create a event when pause a todo' do
+
+      todo.status=1
+      todo.save!
+      todo.status=0
+      todo.save!
+
+      event = Event.last
+      expect(event.body).to eq('paused todo')
+
+    end
+
+
 
   end
 end
